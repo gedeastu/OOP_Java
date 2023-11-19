b7d7657b13169cbb0a3903973ecc5e6b90bdbbd2
gedeastu <gede.astu@gmail.com>
2023-11-19 22:40:54 +0700

    up

--- OOP/src/LatihanAssesment/Burung.java
+++ OOP/src/LatihanAssesment/Burung.java
@@ -5,10 +5,11 @@ public class Burung extends Hewan {
     Scanner scan = new Scanner(System.in);
     String jenisBurung = scan.nextLine();
     String namaBurung = scan.nextLine();
-
+    String suaraBurung = scan.nextLine();
     @Override
     public void bersuara(){
         System.out.println("Nama Hewan : " + namaBurung);
-        System.out.println("Suara Burung : " + jenisBurung);
+        System.out.println("Jenis Burung : " + jenisBurung);
+        System.out.println("Suara Burung : " + suaraBurung);
     }
 }
--- OOP/src/LatihanAssesment/Mamalia.java
+++ OOP/src/LatihanAssesment/Mamalia.java
@@ -1,12 +1,17 @@
 package LatihanAssesment;
 import java.util.Scanner;
+
+
+
 class Mamalia extends Hewan {
     Scanner scan = new Scanner(System.in);
     String jenisMamalia = scan.nextLine();
     String namaMamalia = scan.nextLine();
+    String suaraMamalia = scan.nextLine();
     @Override
     public void bersuara(){
         System.out.println("Nama Hewan : " + namaMamalia);
-        System.out.println("Suara Mamalia : " + jenisMamalia);
+        System.out.println("Jenis Mamalia : " + jenisMamalia);
+        System.out.println("Suara Mamalia : " + suaraMamalia);
     }
 }
